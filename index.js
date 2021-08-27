b768d12f52cb159820480be1f6b3584a5168f0a4
priya-logiciel <87015564+priya-logiciel@users.noreply.github.com>
2021-08-27 18:33:39 +0530

    TASK
    Here, I have updated my task that include -
    1.)Creating Form having input fields named Firstname, Lastname, Age, Address.
    2.)Adding the input field data to the List View by using Dom targeting method.
    3.)Using Clear button to clear the input fields.
    4.)Added Validation to the input fields.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
  var value=[ ]  
-function myFunction(){
+function myFunction(){  
     const a={};
        a['firstname'] = document.getElementById('firstname').value
        a['lastname'] = document.getElementById('lastname').value
@@ -8,9 +8,33 @@ function myFunction(){
      value.push(a)
      console.log(value)
      var list = document.createElement("li");
-     list.innerHTML = value;
+     var g = '<div></div>'
+     list.innerHTML = "Firstname_" +a.firstname + "<br>" +"Lastname_" +a.lastname + "<br>" +"Age_" +a.age + "<br>" +"Address_" +a.address;
      document.getElementById("myUL").appendChild(list);
-}
+    }
+  //clear button
   function clrfrm() {
     document.getElementById("Form").reset();
 }
+  //validation function
+  function validateForm(){
+  var fname  = document.getElementById('fname');
+  var lname = document.getElementById('lname');
+  var age = document.getElementById('age');
+  var address= document.getElementById('address');
+  if(e == ""){
+    alert("name must be filled out")
+  }
+  else if (b == ""){
+     alert("not valied")
+  }
+  else if ( c == ""){
+      alert("data is not valied")
+  }
+  else if (d == ""){
+       alert("must be not out filed")
+  }else{
+
+     }
+       
+  }
