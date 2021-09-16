befe5ade5a77bbd7c7283f052cae7b1e2dfcf623
priya-logiciel <87015564+priya-logiciel@users.noreply.github.com>
2021-09-16 19:03:32 +0530

    task 1
    search button nd use to pagination

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var editId = null;
+var current_page = 1;
+var records_per_page = 3;
 var value = [
   {
     address: "Metro",
@@ -21,8 +23,57 @@ var value = [
     id: 3,
     lname: "kumar",
   },
+  {
+    address: "Metro",
+    age: "19",
+    fname: "priyanka",
+    id: 1,
+    lname: "Kumari",
+  },
+  {
+    address: "Jasiya",
+    age: "24",
+    fname: "geeta",
+    id: 2,
+    lname: "Kumari",
+  },
+  {
+    address: "Tajpur",
+    age: "25",
+    fname: "Ronit",
+    id: 3,
+    lname: "kumar",
+  },
+  {
+    address: "Metro",
+    age: "24",
+    fname: "Priyu",
+    id: 1,
+    lname: "Kumari",
+  },
+  {
+    address: "Jasiya",
+    age: "25",
+    fname: "Shiva",
+    id: 2,
+    lname: "Kumar",
+  },
+  {
+    address: "Tajpur",
+    age: "26",
+    fname: "Rinku",
+    id: 3,
+    lname: "kumar",
+  },
+  {
+    address: "Tajpur",
+    age: "27",
+    fname: "Rohan",
+    id: 3,
+    lname: "kumar",
+  },
 ];
-
+//load function
 function loadData() {
   value.forEach((item) => {
     var newElement = document.createElement("li");
@@ -42,6 +93,7 @@ function loadData() {
       item.address +
       "<br>";
     // <button onClick=editList("+ item.id +")>Edit</button><button onClick=deleteList("+ item.id +")>delete</button>
+    //edit button
     let editbutton = document.createElement("button");
     editbutton.innerText = "Edit";
     editbutton.addEventListener("click", function () {
@@ -60,6 +112,7 @@ function loadData() {
     document.getElementById("list").appendChild(newElement);
   });
 }
+// add function
 function add() {
   const b = {};
   b["fname"] = document.getElementById("fname").value;
@@ -105,7 +158,7 @@ function add() {
       "Address-" +
       b.address +
       "<br>";
-    //<button onClick=editList()>Edit</button><button onClick=deleteList("+ b.id +")>delete</button>";
+    //edit button
     let editbutton = document.createElement("button");
     editbutton.innerText = "Edit";
     editbutton.addEventListener("click", function () {
@@ -123,16 +176,17 @@ function add() {
     newElement.appendChild(deletebutton);
     document.getElementById("list").appendChild(newElement);
   }
-
+  //clear
   document.getElementById("form1").reset();
 }
+//delate function
 function deleteList(id) {
   let index = value.findIndex((el) => el.id == id);
   value.splice(index, 1);
   var delList = document.getElementById("list");
   delList.removeChild(delList.childNodes[index]);
 }
-
+//edit function
 var selectedListItem = null;
 function editList(id) {
   let index = value.findIndex((el) => el.id == id);
@@ -142,16 +196,11 @@ function editList(id) {
   document.getElementById("address").value = value[index].address;
   editId = id;
 }
-
+// clear function
 function clrfrm() {
   document.getElementById("form1").reset();
 }
-//search button function
-function Search() {
-  var list3 = document.getElementById("myInput");
-  document.getElementById("list").innerHTML = list3.value;
-}
-
+//validation function
 function validateForm() {
   var fname = document.getElementById("fname");
   var lname = document.getElementById("lname");
@@ -169,3 +218,76 @@ function validateForm() {
     add();
   }
 }
+//search function
+function Search() {
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("list");
+  li = ul.getElementsByTagName("li");
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("div")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
+//pagination function
+function prevPage() {
+  if (current_page > 1) {
+    current_page--;
+    changePage(current_page);
+  }
+}
+
+function nextPage() {
+  if (current_page < numPages()) {
+    current_page++;
+    changePage(current_page);
+  }
+}
+
+function changePage(page) {
+  var btn_next = document.getElementById("btn_next");
+  var btn_prev = document.getElementById("btn_prev");
+  var listing_table = document.getElementById("listingTable");
+  var page_span = document.getElementById("page");
+
+  // Validate page
+  if (page < 1) page = 1;
+  if (page > numPages()) page = numPages();
+
+  listing_table.innerHTML = "";
+
+  for (
+    var i = (page - 1) * records_per_page;
+    i < page * records_per_page && i < value.length;
+    i++
+  ) {
+    listing_table.innerHTML += value[i].adName + "<br>";
+  }
+  page_span.innerHTML = page + "/" + numPages();
+
+  if (page == 1) {
+    btn_prev.style.visibility = "hidden";
+  } else {
+    btn_prev.style.visibility = "visible";
+  }
+
+  if (page == numPages()) {
+    btn_next.style.visibility = "hidden";
+  } else {
+    btn_next.style.visibility = "visible";
+  }
+}
+
+function numPages() {
+  return Math.ceil(value.length / records_per_page);
+}
+
+window.onload = function () {
+  changePage(1);
+};
